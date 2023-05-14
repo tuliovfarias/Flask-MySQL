8b38c8981c2eee01d582018cfe4caa51ec931034
tuliovfarias <tulio-4ever@hotmail.com>
2023-05-14 10:50:42 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import pymysql
+
+app = Flask(__name__)
+
+# MySQL database configuration
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'computer',
+    'db': 'db_test'
+}
+
+
+@app.route('/')
+def index():
+    # MySQL connection
+    conn = pymysql.connect(**db_config)
+
+    # Query to retrieve all data from the table
+    query = "SELECT * FROM table_test"
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    # Fetch all data
+    data = cursor.fetchall()
+
+    # Get the field names from the cursor description
+    fields = [field[0] for field in cursor.description]
+
+    # Pass the length of the fields list as a separate variable
+    fields_length = len(fields)
+
+    # Close the MySQL connection
+    conn.close()
+
+    # Render the template with the data and field names
+    return render_template('index.html', data=data, fields=fields, fields_length=fields_length)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
